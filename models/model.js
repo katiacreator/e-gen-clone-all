7d6ea466b537bf0cc08bdf8387b0bf8d4ac3bf6b
Katia Gibson <kcygibson@gmail.com>
2021-07-28 09:07:43 -0400

    import mongoose, export model, mongoose schema shortcut and connect model to mongoose in models/model.js

--- models/model.js
+++ models/model.js
@@ -1,5 +1,18 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
+
 
 const Schema = mongoose.Schema
+export {
+Model
+}
 
-export { Model }
+const modelSchema = new Schema ({
+  name: String,
+  boolean: Boolean,
+  number: Number
+},{
+  timestamps: true 
+})
+
+
+const Model = mongoose.model("Model", modelSchema)
