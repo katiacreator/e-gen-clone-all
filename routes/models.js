e23b6e8589af94de4db18b6c8b69426c74a35fff
Katia Gibson <kcygibson@gmail.com>
2021-07-28 09:12:44 -0400

    set up router for model and import controller, set up view files for model

--- routes/models.js
+++ routes/models.js
@@ -1,5 +1,10 @@
 import { Router } from 'express'
 const router = Router()
+import * as modelsCtrl from "../controllers/models.js"
+
+router.get('/', modelsCtrl.index)
+// router.post('/', modelsCtrl.create)
+// router.delete('/:id', modelsCtrl.delete)
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
