43a7aa381bb7a4cc7c13369b96104d1412b69166
Katia Gibson <kcygibson@gmail.com>
2021-07-28 08:57:30 -0400

    install packages and import all to server and connect db in all places

---
+++ server.js
@@ -0,0 +1,58 @@
+import("dotenv/config.js")//if bug check this
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+//if bug check this
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+//if bug check this
+import methodOverride from 'method-override'
+import ('./config/database.js')//if bug check this
+//if bug check this
+import { router as indexRouter } from './routes/index.js'
+import { router as usersRouter } from './routes/users.js'
+//if bug check this
+import ('./config/database.js')
+
+const app = express()
+
+// view engine setup
+app.set(
+  'views',
+  path.join(path.dirname(fileURLToPath(import.meta.url)), 'views')
+)
+app.set('view engine', 'ejs')
+//if bug check this
+app.use(methodOverride('_method'))
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+//if bug check this
+app.use(cookieParser())
+app.use(
+  express.static(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
+  )
+)
+//if bug check this
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+export { app }
