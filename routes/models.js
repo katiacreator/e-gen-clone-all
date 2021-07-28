c5871e9f83b9e7952e853bfbef3343dd3dedb430
Katia Gibson <kcygibson@gmail.com>
2021-07-28 09:19:58 -0400

    stubbed up all common routers in routes

--- routes/models.js
+++ routes/models.js
@@ -2,9 +2,16 @@ import { Router } from 'express'
 const router = Router()
 import * as modelsCtrl from "../controllers/models.js"
 
-router.get('/', modelsCtrl.index)
-// router.post('/', modelsCtrl.create)
-// router.delete('/:id', modelsCtrl.delete)
+// localhost:3000/api/models RETRIEVE ALL COLLECTIONS
+router.get('/models', modelsCtrl.index)
+// localhost:3000/api/models/:id RETRIEVE A SINGLE COLLECTION
+router.get('/models/:id', modelsCtrl.show)
+// localhost:3000/api/models ADD A DOCUMENT TO COLLECTION
+router.post('/models', modelsCtrl.create)
+// localhost:3000/api/models/:id UPDATE A SINGLE DOCUMENT
+router.put('/models/:id', modelsCtrl.update)
+// localhost:3000/api/models/:id DELETE A SINGLE DOCUMENT
+router.delete('/models/:id', modelsCtrl.delete)
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
